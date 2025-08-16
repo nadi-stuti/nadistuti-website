2c7470b95f3e63f64ff37bf3220c11cb15222361
Nulligma <shantanu@nulligma.com>
2025-08-16 06:37:38 +0530

    fixed moderate severity vulnerability and taildwind issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nadistuti.com",
+
   i18n: {
     defaultLocale: "en",
     locales: [
@@ -29,8 +31,10 @@ export default defineConfig({
       prefixDefaultLocale: false,
     },
   },
+
+  integrations: [react(), sitemap()],
+
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [react(), sitemap()],
 });
